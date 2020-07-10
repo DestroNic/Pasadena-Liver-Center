dfb97858cd38d121f29e572b865cbca3ca019b8a
Ricardo Maldonado <rcmalonso@gmail.com>
2020-07-09 23:57:52 -0700

    contact page ready

---
+++ src/components/map.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ConnectApiMaps,{Map} from 'maps-google-react';
+ 
+export class MapContainer extends React.Component {
+    render() {
+        return (
+          <Map 
+          google={this.props.google} 
+          mapOptions={{
+                            zoom: 15,
+                            center: { lat: 34.140545, lng: -118.150909 },
+                            disableDefaultUI: true,
+                            styles: [{
+                                featureType: 'poi.business',
+                                stylers: [{ visibility: 'on' }]
+                            },
+                            {
+                                featureType: 'transit',
+                                elementType: 'labels.icon',
+                                stylers: [{ visibility: 'off' }]
+                            }]
+                        }}>
+          </Map>
+        );
+      }
+}
+ 
+export default ConnectApiMaps({
+  apiKey: ("AIzaSyAnkcfEFGnzGGaLksiKC4jiGDYSDgM3qh0")
+})(MapContainer)
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,72 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import Header from '../components/header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faClock, faPhoneAlt, faFax } from '@fortawesome/free-solid-svg-icons';
+import MapContainer from '../components/map';
+
+const title = "Contact Us";
+
+
 
 const Contact = () => {
     return(
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <motion.div>
+            <Header header={title} />
+            <div className="contact-wrapper">
+                <div className="contact-info">
+                    <div className="left-side">
+                        <div className="contact-item">
+                            <div className="icon-wrapper">
+                                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                            </div>
+                            <div className="contact-detail">
+                                
+                                <p>Address</p>
+                               <p>301 S. Fair Oaks Ave.  Ste 405<br /> Pasadena CA 91105</p>
+
+                            </div>
+                        </div>
+                        <div className="contact-item">
+                            <div className="icon-wrapper">
+                                <FontAwesomeIcon icon={faClock} />
+                            </div>
+                            <div className="contact-detail">
+                                <p>Office Hours</p>
+                                <p>Monday - Friday – 9:00 to 5:00pm</p>
+                            </div>
+                        </div>
+                        <div className="contact-item">
+                            <div className="icon-wrapper">
+                                <FontAwesomeIcon icon={faPhoneAlt} />
+                            </div>
+                            <div className="contact-detail">
+                                <p>Phone</p>
+                                <p>626-440-7325</p>
+                            </div>
+                        </div>
+                        <div className="contact-item">
+                            <div className="icon-wrapper">
+                                <FontAwesomeIcon icon={faFax} />
+                            </div>
+                            <div className="contact-detail">
+                                <p>Fax</p>
+                                <p>626-440-7310</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right-side">
+                        <MapContainer />
+                    </div>
+                </div>
+                <div className="contact-footer">
+                    <p>Fibroscan (By appointment only)</p><br />
+                    <p>Tuesday, Wednesday, and Fridays – 8:45am to 10:30am <br />
+Tuesday Afternoon – 1:00pm to 2:00pm (Outside referral or no follow up needed)</p>
+
+                </div>
+            </div>
+        </motion.div>
 
     )
 }
